562bb9ae1c649cb55c7944e7c3f170494f2409c8
parthi.deva <kparthi@joshbrains.com>
2023-11-23 12:33:55 +0530

    notes

--- ConsoleApp1/InterfaceCrudClass.cs
+++ ConsoleApp1/InterfaceCrudClass.cs
@@ -1,12 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SISTrainingDotNet
 {
-    internal class InterfaceCrudClass
+    class InterfaceCrudClass
     {
+        //object creation 
+
+    }
+
+
+
+    class ParentCompany : ICompany
+    {
+        public void CreateCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCompany()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class VendorCompany : ICompany
+    {
+        public void CreateCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCompany()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class SisterCompany : ICompany
+    {
+        public void CreateCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCompany()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    interface ICompany
+    {
+        void LoadCompany();
+        void CreateCompany();
+        void UpdateCompany();
+        void ReadCompany();
+        void DeleteCompany();
     }
 }
