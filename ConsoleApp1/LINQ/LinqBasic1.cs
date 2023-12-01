d4162ad5b0a02707d25fae1ec727a517534ad739
parthi.deva <kparthi@joshbrains.com>
2023-12-01 12:34:36 +0530

    Linq Basics 01122023-

---
+++ ConsoleApp1/LINQ/LinqBasic1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISTrainingDotNet.LINQ
+{
+    class LinqBasic1
+    {
+
+
+        //static void Main(string[] args)
+        //{
+        //    string[] Students = { "Gopi", "Namrata", "Priya", "Sairam", "Allwyn" };
+
+        //    foreach(var student in Students) 
+        //    {
+        //        if(student =="Priya")
+        //        {
+        //            Console.WriteLine("Priya Student is there in list");
+        //        }
+        //    }
+
+
+        //    //from < range variable > in < IEnumerable < T > or IQueryable<T> Collection>
+
+        //    //< Standard Query Operators> < lambda expression >
+
+        //    //< select or groupBy operator> < result formation >
+
+
+        //    var Std = from Student in Students
+        //              where Student == "Priya" || Student == "Gopi"
+        //              select Student;
+
+
+
+        //}
+
+    }
+
+}
---
+++ ConsoleApp1/LINQ/StudentLinq.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISTrainingDotNet.LINQ
+{
+     class StudentLinq
+    {
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students = StudentMain.LoadStudent();
+
+            var FilterStdId = from std in students
+                              where std.Id == 1001
+                              select std;
+        }
+
+
+       
+    }
+
+
+    class StudentMain
+    {
+        public static List<Student> LoadStudent()
+        {
+
+            List<Student> students = new List<Student>() 
+            {
+                new Student(){Id=1000,Name="Peter",Email="Peter@gmail.com",Address="Texas,USA"},
+                 new Student(){Id=1001,Name="Mark",Email="Mark@gmail.com",Address="Munich,German"},
+                  new Student(){Id=1002,Name="Matt",Email="Matt@gmail.com",Address="Lords,UK"},
+                   new Student(){Id=1003,Name="Rithik",Email="Rithik@gmail.com",Address="Bangalore,India"}
+            };
+
+            return students;
+        }
+
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+            public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
